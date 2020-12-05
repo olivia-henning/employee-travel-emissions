5b98ebcac71372eed03c631f69e5c28e9c328cd8
Lucia V <luciamvila@gmail.com>
2020-12-05 13:17:33 -0500

    added scripts, json

---
+++ scrapers/R/server-attempt/request2.sh
@@ -0,0 +1,2 @@
+curl -X POST -H "Content-Type: application/json" -d @./request2.json https://applications.icao.int/icec/Home/ChildMapView
+
---
+++ scrapers/R/server-attempt/request3.sh
@@ -0,0 +1,2 @@
+curl -X POST -H "Content-Type: application/json" -d @./request3.json https://applications.icao.int/icec/Home/ChildResultTotalView
+
